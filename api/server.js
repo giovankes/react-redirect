bc75291f2c8917b5cf4f520a809ac43d59a5daca
Giotje <devgio@icloud.com>
2021-03-08 15:15:15 +0100

    [chore] did some refactoring

--- api/server.js
+++ api/server.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const bodyparser = require('body-parser');
+const express = require("express");
+const bodyparser = require("body-parser");
 const app = express();
 // middleware
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
-app.post('/wp/redirections', (req, res) => {
-  req.on('close', () => {
+app.post("/wp/redirections", (req, res) => {
+  req.on("close", () => {
     console.log(req.body);
     res.end();
   });
 });
 
-app.listen(3002, () => console.log(`App is running`));
+app.listen(process.env.PORT || 3002, () => {
+  console.log(`API running on port ${process.env.PORT || 3002} 🚀`);
+});
