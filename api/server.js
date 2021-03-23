aa75c90a653d3a468b6445baddf54137086d299a
Giotje <devgio@icloud.com>
2021-03-23 15:58:09 +0100

    [ID and redirection are now seperate rows]

--- api/server.js
+++ api/server.js
@@ -10,6 +10,6 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.post("/wp/redirections", async (req, res) => {
   const redirection = new Redirection_DB({ redirection: req.body });
 
-  redirection.__init__();
+  redirection.write();
 });
 app.listen(3002, () => console.log(`API is running on port 3002 🚀`));
--- api/src/Redirection_DB.js
+++ api/src/Redirection_DB.js
@@ -1,25 +1,34 @@
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("hike_red");
 const fs = require("fs");
+
+let sql_select = `SELECT * FROM IF EXISTS redirection`;
 class Redirection_DB {
   constructor({ redirection }) {
     this.redirection = redirection;
   }
-
-  __init__() {
+  //This'll write the new redirection to the database
+  //@TODO: Only write new redirections.
+  write() {
+    let ID = this.redirection.id;
+    console.log(this.redirection);
     let json_red = JSON.stringify(this.redirection);
     db.serialize(function () {
-      db.run("CREATE TABLE IF NOT EXISTS redirection (redirections TEXT)");
+      db.run(
+        "CREATE TABLE IF NOT EXISTS redirection (id TEXT, redirection TEXT)"
+      );
+      const stmt = db.prepare(`INSERT INTO redirection VALUES(?,?)`);
 
-      var stmt = db.prepare("INSERT INTO redirection VALUES(?)");
-      stmt.run(`"'${json_red}'"`);
+      stmt.run(`"${ID}"`, `"${json_red}"`);
       stmt.finalize();
     });
-  }
-
-  write() {
-    console.log("lmao");
+    // db.all(sql_select, [], (e, r) => {
+    //   if (e) throw e;
+    //   if (r.length <= 0) return;
+    //   r.forEach((row) => {
+    //     console.log(row.redirections);
+    //   });
+    // });
   }
 }
-
 module.exports = Redirection_DB;
