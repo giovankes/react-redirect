5c23f7e5fa9d92ac5ce2dd8cf93a964e738e19ca
Giotje_ <devgio@icloud.com>
2021-03-25 13:26:44 +0100

    [refactor]

--- src/main/DB_Model.js
+++ src/main/DB_Model.js
@@ -1,13 +1,45 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("hike_red");
 
-class DB_Model{
-  constructor({data}){
+class DB_Model {
+  constructor({ data }) {
     this.data = data;
   }
-  
-   update_redirect(){
-     let redirection = this.data;
+
+  update_redirect(id) {
+    let redirection = this.data;
+    let json_red = JSON.stringify(redirection);
+    let duplicate_redirection = false;
+    db.all("SELECT * FROM redirections", (e, r) => {
+      r.map((result, key) => {
+        if (result.id === id) {
+          duplicate_redirection = true;
+        } else {
+          console.log("no");
+        }
+      });
+      console.log(duplicate_redirection);
+      if (duplicate_redirection === true) {
+        //if the redirection is a dupe we want to update the appropiate
+        //redirection, instead of adding  a new one
+        //@@TODO: update redirection
+        db.exec(
+          `UPDATE redirections SET redirection = '${json_red}' WHERE id = '${ID}'`,
+          (e) => {
+            if (e) throw e;
+            console.log("Updated");
+          }
+        );
+      } else {
+        //is it a new redirection? cool, then we just insert it into the
+        //database
+        const stmt = db.prepare(`INSERT INTO redirections VALUES(?,?)`);
+        stmt.run(`${id}`, `${json_red}`);
+        stmt.finalize();
+        console.log("done");
+      }
+    });
   }
-  
 }
 
-module.exports=  DB_Model
+module.exports = DB_Model;
--- src/main/Redirection_DB.js
+++ src/main/Redirection_DB.js
@@ -1,7 +1,7 @@
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("hike_red");
 const DB_Model = require("./DB_Model.js");
- class Redirection_DB {
+class Redirection_DB {
   constructor({ redirection }) {
     this.redirection = redirection;
   }
@@ -13,45 +13,10 @@ const DB_Model = require("./DB_Model.js");
     //Check if id is already in the database and insert || update accordingly
     let json_red = JSON.stringify(this.redirection);
 
-    let redirect = new DB_Model({ data: this.redirection });
+    let dbmodel = new DB_Model({ data: this.redirection });
 
-    redirect.update_redirect();
-
-    //db.serialize(function () {
-    //  db.exec(
-    //    "CREATE TABLE IF NOT EXISTS redirections (id INTEGER PRIMARY KEY, redirection TEXT)"
-    //  );
-    //  db.all("SELECT * FROM redirections", (e, r) => {
-    //    r.map((result, key) => {
-    //      if (result.id === ID) {
-    //        duplicate_redirection = true;
-    //      } else {
-    //        console.log("no");
-    //      }
-    //    });
-    //    console.log(duplicate_redirection);
-    //    if (duplicate_redirection === true) {
-    //      //if the redirection is a dupe we want to update the appropiate
-    //      //redirection, instead of adding  a new one
-    //      //@@TODO: update redirection
-    //      db.exec(
-    //        `UPDATE redirections SET redirection = '${json_red}' WHERE id = '${ID}'`,
-    //        (e) => {
-    //          if (e) throw e;
-    //          console.log("Updated");
-    //        }
-    //      );
-    //    } else {
-    //      //is it a new redirection? cool, then we just insert it into the
-    //      //database
-    //      const stmt = db.prepare(`INSERT INTO redirections VALUES(?,?)`);
-    //      stmt.run(`${ID}`, `${json_red}`);
-    //      stmt.finalize();
-    //      console.log("done");
-    //    }
-    //  });
-    //});
+    dbmodel.update_redirect({ id: ID });
   }
 }
 
-module.exports = Redirection_DB
+module.exports = Redirection_DB;
