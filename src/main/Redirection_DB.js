71d2815cc20aa2a5db8eb0b2aaad04f8b45d50bb
Giotje <devgio@icloud.com>
2021-03-29 14:50:35 +0200

    [worked on the tests and overal structure of the package]

--- src/main/Redirection_DB.js
+++ src/main/Redirection_DB.js
@@ -9,12 +9,18 @@ class Redirection_DB {
   //@TODO: Only write new redirections.
   //
   __init__() {
+    let results;
     db.exec(
       "CREATE TABLE IF NOT EXISTS redirections (id INTEGER PRIMARY KEY, redirection TEXT)",
       (e) => {
         if (e) throw e;
+        console.log("database initialized");
       }
     );
+    db.all("SELECT * FROM redirections", (e, r) => {
+      if (e) throw e;
+      console.log(r);
+    });
   }
 
   write_redirect() {
@@ -30,6 +36,24 @@ class Redirection_DB {
 
     console.log(ID);
     //dbmodel.check(ID);
+  }
+
+  //This functions is ONLY used when running tests. Please don't change this
+  //code, and if you do: Please make sure all the tests pass again :)
+  test() {
+    let results;
+    db.exec(
+      "CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, redirection TEXT)",
+      (e) => {
+        if (e) throw e;
+        console.log("database initialized");
+      }
+    );
+    db.all("SELECT * FROM test", (e, r) =>{
+      if(e) throw e;
+      console.log(r);
+    })
+
   }
 }
 
--- test/index.js
+++ test/index.js
@@ -1,23 +1,7 @@
-let Redirection_DB = require("../api/src/Redirection_DB.js");
+let Redirection_DB = require("../src/main/Redirection_DB.js");
 const test_json = require("./test.json");
 const fs = require("fs");
-//describe("Test", () => {
-//  let redirection;
-//  before(() => {
-//    // @TODO this would be a better name. Write_json is wrong because it is not what the class is about
-//    redirection = new Redirection_DB({ redirection: test_json });
-//  });
-//  it("Creates new redirection.json with one redirect added", () => {
-//    // @TODO assertions go here
-//    const raw_json = fs.readFileSync("./test/test.json");
-//    const parsed_json = JSON.parse(raw_json);
-//  });
-//  it("Serializes database and input data in the table", () => {
-//    redirection.__init__();
-//  });
-//});
-//
-//
+
 // This test will initialize the database and see if everything works
 // correctly. (Like a test should :P)
 //
@@ -33,6 +17,6 @@ describe("Redirect test", () => {
     const raw_json = fs.readFileSync("./test/test.json");
     const parsed_json = JSON.parse(raw_json);
 
-    db.run("")
+   redirection.__init__(); 
   })
 });
